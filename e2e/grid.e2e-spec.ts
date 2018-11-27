241ac24025824f69a43a2dbb2d6d86a70bb182a6
Alex Kingman <alex.kingman@blackbaud.com>
2018-11-27 17:06:18 -0500

    added visual tests

--- e2e/grid.e2e-spec.ts
+++ e2e/grid.e2e-spec.ts
@@ -72,4 +72,40 @@ describe('Grid', () => {
     element(by.css('#highlight-button')).click();
     expect('#screenshot-grid').toMatchBaselineScreenshot(done);
   });
+
+  it('should match previous screenshot with multiselect enabled', (done) => {
+    SkyHostBrowser.get('visual/grid');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-grid-multiselect');
+    expect('#screenshot-grid-multiselect').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous screenshot with multiselect enabled (screen: xs)', (done) => {
+    SkyHostBrowser.get('visual/grid');
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    SkyHostBrowser.scrollTo('#screenshot-grid-multiselect');
+    expect('#screenshot-grid-multiselect').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous screenshot with selected rows', (done) => {
+    SkyHostBrowser.get('visual/grid');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    SkyHostBrowser.scrollTo('#screenshot-grid-multiselect');
+    let checkboxes = element.all(by.css('.sky-grid-multiselect-cell sky-checkbox'));
+    checkboxes.get(0).click();
+    checkboxes.get(2).click();
+    checkboxes.get(3).click();
+    expect('#screenshot-grid-multiselect').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous screenshot with selected rows (screen: xs)', (done) => {
+    SkyHostBrowser.get('visual/grid');
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    SkyHostBrowser.scrollTo('#screenshot-grid-multiselect');
+    let checkboxes = element.all(by.css('.sky-grid-multiselect-cell sky-checkbox'));
+    checkboxes.get(0).click();
+    checkboxes.get(2).click();
+    checkboxes.get(3).click();
+    expect('#screenshot-grid-multiselect').toMatchBaselineScreenshot(done);
+  });
 });
