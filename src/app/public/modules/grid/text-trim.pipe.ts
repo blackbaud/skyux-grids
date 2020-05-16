c05185074c71a227cbe0f3fea01cab9cca7b30d0
Alex Vaky <alexander.vaky@blackbaud.com>
2020-05-15 21:53:37 -0500

    fix typo for pipe declaration

--- src/app/public/modules/grid/text-trim.pipe.ts
+++ src/app/public/modules/grid/text-trim.pipe.ts
@@ -3,7 +3,7 @@ import {
   PipeTransform
 } from '@angular/core';
 
-@Pipe({name: 'texTrim'})
+@Pipe({name: 'textTrim'})
 export class TextTrimPipe implements PipeTransform {
   public transform(value: string): string {
     return value ? value.trim() : '';
